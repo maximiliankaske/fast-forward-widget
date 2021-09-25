bae8b205eebd21be1d6513f6f9ebdc9461e262e5
Maximilian Kaske <maximilian@kaske.org>
2021-09-25 16:12:13 +0200

    chore: config tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import './tailwind.css'
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Fast Forward! {text}</div>
+  return (
+    <h1 className='text-gray-500 font-medium text-4xl'>Fast Forward: {text}</h1>
+  )
 }
