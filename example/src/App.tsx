48cd8b09c1ed430519c434d8088befac2dfe8345
Maximilian Kaske <maximilian@kaske.org>
2021-09-25 17:35:31 +0200

    feat: update example App

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
-import { ExampleComponent } from 'fast-forward-widget'
+import { FFWidget } from 'fast-forward-widget'
 import 'fast-forward-widget/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='mx-auto max-w-xl'>
+      <div className='p-6 space-y-6'>
+        <h1 className='text-indigo-500 font-medium text-4xl'>Test App</h1>
+        <div>
+          <FFWidget
+            projectId='VWJU7eJdIEYGmoyKW4rp'
+            domain='https://fast-forward-p0f7wt0qd-mxkaske.vercel.app'
+          >
+            <button className='rounded-full text-white bg-black font-medium leading-6 py-2 px-4'>
+              Give use Feedback!
+            </button>
+          </FFWidget>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
